1ca5477797633545d41d58b16bd33186a84a04ce
Julia Grace <jewelia@gmail.com>
2013-09-29 20:35:15 -0700

    Adding source

---
+++ requirements.py
@@ -0,0 +1,3 @@
+pyFirmata==0.9.5
+BreakfastSerial==0.0.9
+twilio==3.6.2
---
+++ settings.py
@@ -0,0 +1,5 @@
+twilio_account_sid = 'PUT YOUR TWILIO ACCT SID HERE'
+twilio_auth_token = 'PUT YOUR TWILIO AUTH TOKEN HERE'
+your_phone_number = 'PUT YOUR PHONE NUMBER HERE'
+your_twilio_number = 'PUT YOUR TWILIO PHONE NUMBER HERE'
+
